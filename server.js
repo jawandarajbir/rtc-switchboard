06a3589504c3598aa80276b97f6c5de20ea4552c
Rajbir Jawanda <jawandarajbir@gmail.com>
2016-10-10 13:06:42 +1100

    Simplify Server.js

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
-var server = require('http').createServer();
-var switchboard = require('./')(server, { servelib: true });
-var port = parseInt(process.env.NODE_PORT || process.env.PORT || process.argv[2], 10) || 3000;
+﻿var http = require('http');
+var port = process.env.port || 1337;
 var host = process.env.NODE_HOST || process.env.HOST || 'localhost';
-// var replify = require('replify');
+var replify = require('replify');
 
-server.on('request', function(req, res) {
-  if (req.url === '/') {
-    res.writeHead(302, {
-      'Location': 'https://github.com/rtc-io/rtc-switchboard'
-    });
-    res.end('switchboard available from: https://github.com/rtc-io/rtc-switchboard');
-  }
-});
-
-// start the server
-server.listen(port, host, function(err) {
-  if (err) {
-    return console.log('Encountered error starting server: ', err);
-  }
-
-  console.log('server running at http://' + host + ':' + port + '/');
-});
-
-// // add the repl
-// replify({
-//   name: 'switchboard',
-//   app: switchboard,
-//   contexts: {
-//     server: server
-//   }
-// });
-//
-// switchboard.on('room:create', function(room) {
-//   console.log('room ' + room + ' created, now have ' + switchboard.rooms.length + ' active rooms');
-// });
-//
-// switchboard.on('room:destroy', function(room) {
-//   console.log('room ' + room + ' destroyed, ' + switchboard.rooms.length + ' active rooms remain');
-//
-//   if (typeof gc == 'function') {
-//     console.log('gc');
-//     gc();
-//   }
-// });
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('rtc switchboard is running on'
+        + ' \n port:' + port
+        + ' \n host:' + host);
+}).listen(port);
+var switchboard = require('./')(server, { servelib: true });
